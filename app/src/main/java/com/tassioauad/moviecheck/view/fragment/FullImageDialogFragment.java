8d3294d1dfb2ffdd39c39f977ff345f8c2446e0a
Tássio Auad <auadtassio@gmail.com>
2016-05-05 16:20:09 -0300

    feat: Showing images in a full screen Able to show images in a fullscreen mode.

--- app/src/main/java/com/tassioauad/moviecheck/view/fragment/FullImageDialogFragment.java
+++ app/src/main/java/com/tassioauad/moviecheck/view/fragment/FullImageDialogFragment.java
@@ -17,8 +17,6 @@ import com.tassioauad.moviecheck.R;
 public class FullImageDialogFragment extends DialogFragment {
 
     private static final String BUNDLE_ARG_PHOTOURL = "bundle_arg_photourl";
-    private float mx;
-    private float my;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -29,36 +27,6 @@ public class FullImageDialogFragment extends DialogFragment {
         Picasso.with(getActivity()).load(getArguments().getString(BUNDLE_ARG_PHOTOURL))
                 .placeholder(R.drawable.noimage).into(imageViewPhoto);
         alertDialogBuilder.setView(view);
-        imageViewPhoto.setOnTouchListener(new View.OnTouchListener() {
-
-            public boolean onTouch(View arg0, MotionEvent event) {
-
-                float curX, curY;
-
-                switch (event.getAction()) {
-
-                    case MotionEvent.ACTION_DOWN:
-                        mx = event.getX();
-                        my = event.getY();
-                        break;
-                    case MotionEvent.ACTION_MOVE:
-                        curX = event.getX();
-                        curY = event.getY();
-                        imageViewPhoto.scrollBy((int) (mx - curX), (int) (my - curY));
-                        mx = curX;
-                        my = curY;
-                        break;
-                    case MotionEvent.ACTION_UP:
-                        curX = event.getX();
-                        curY = event.getY();
-                        imageViewPhoto.scrollBy((int) (mx - curX), (int) (my - curY));
-                        break;
-                }
-
-                return true;
-            }
-        });
-
         AlertDialog alertDialog = alertDialogBuilder.create();
         alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         return alertDialog;
